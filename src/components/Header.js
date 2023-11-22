e672f8038a2653f3f69fc2388b87adc495b23454
setfocusbrasil <128048171+qualityseg@users.noreply.github.com>
2023-11-22 12:09:25 -0300

    branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,9 +18,7 @@ const Header = () => {
       <li><Link to="/tecnologia">Tecnologia</Link></li>
       <li><Link to="/contato">Fale Conosco</Link></li>
       <li><Link to="/a-qualityseg">A Lifemed</Link></li>
-      <li><Link to="https://psico-painel.vercel.app/Login">Login</Link></li></div>
-      
-      
+      <li><Link to="https://psico-painel.vercel.app/Login">Login</Link></li></div>      
     </ul>
   );
 
