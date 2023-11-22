8ba91427e78c36fee91d0fed99a50485af023018
setfocusbrasil <128048171+qualityseg@users.noreply.github.com>
2023-11-22 11:59:35 -0300

    metalurgica

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,11 +21,7 @@ const Header = () => {
       <li><Link to="/a-qualityseg">A Lifemed</Link></li>
       <li><Link to="https://psico-painel.vercel.app/Login">Login</Link></li>
       
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/tecnologia">Tecnologia</Link></li>
-      <li><Link to="/contato">Fale Conosco</Link></li>
-      <li><Link to="/a-qualityseg">A Lifemed</Link></li>
-      <li><Link to="https://psico-painel.vercel.app/Login">Login</Link></li>
+
     </ul>
   );
 
