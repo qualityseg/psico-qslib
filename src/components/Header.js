34f424f1a576a07d71bc4990d13684537f271598
setfocusbrasil <128048171+qualityseg@users.noreply.github.com>
2023-11-22 12:01:21 -0300

    massa

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,16 +12,14 @@ const Header = () => {
 
   const Menu = () => (
     <ul className={isMobile ? "mobile" : "desktop"}>
-
        
-        <img src={qualityFooter} alt="LIA Lab" className="footer-image"/>
-        <li><Link to="/">Home</Link></li>
+      <img src={qualityFooter} alt="LIA Lab" className="footer-image"/>
+      <li><Link to="/">Home</Link></li>
       <li><Link to="/tecnologia">Tecnologia</Link></li>
       <li><Link to="/contato">Fale Conosco</Link></li>
       <li><Link to="/a-qualityseg">A Lifemed</Link></li>
       <li><Link to="https://psico-painel.vercel.app/Login">Login</Link></li>
       
-
     </ul>
   );
 
